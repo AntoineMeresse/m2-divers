745d4b750f0c52f57dd152c73b44b073d8743939
Antoine Meresse <ant.meresse@gmail.com>
2020-11-20 17:36:04 +0100

    feat Tableau

--- web_avance/exo1/exo1.js
+++ web_avance/exo1/exo1.js
@@ -4,3 +4,11 @@ console.log("Hey ! Exo1");
 function randomNumber(xmax){
     return Math.floor(Math.random()*xmax) + 1;
 }
+
+function Tableau(props) {
+    this.conteneurHTML = props.conteneurHTML;
+    this.nbColonnes = props.nbColonnes;
+    this.nbLignes = props.nbLignes;
+    this.styleCellule = props.styleCellule;
+    this.styleTable = props.styleTable;
+}
