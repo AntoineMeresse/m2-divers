8c7b490351fcaac516bd9a3209fbf0bc7d4e38c0
Antoine Meresse <ant.meresse@gmail.com>
2020-12-01 18:13:10 +0100

    chore: fix error in data-service.ts

--- frameworkJS/cat-app/src/app/data-service.ts
+++ frameworkJS/cat-app/src/app/data-service.ts
@@ -21,7 +21,7 @@ export const initialCards: Card[] = [
 ];
 
 function initData() {
-  const cards: Card[] | null = JSON.parse(sessionStorage.getItem('cards'));
+  const cards: Card[] | null = JSON.parse(sessionStorage.getItem('cards') || '{}');
   if(!cards) {
     sessionStorage.setItem('cards', JSON.stringify(initialCards));
   }
@@ -30,87 +30,13 @@ function initData() {
 export function getAllCards(): Promise<Card[]> {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      const cards: Card[] | null = JSON.parse(sessionStorage.getItem('cards'));
+      const cards: Card[] | null = JSON.parse(sessionStorage.getItem('cards') || '{}');
       if(cards && cards.length) {
         resolve(cards);
       } else {
         reject('No cards found !');
       }
     }, 300);
-  });
-}
-
-export function getCard(cardId): Promise<Card> {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      const cards: Card[] | null = JSON.parse(sessionStorage.getItem('cards'));
-      if(cards && cards.length) {
-        const card = cards.find(c => c.id == cardId);
-        if (card) {
-          resolve(card);
-        } else {
-          reject(`Card with id <${cardId}> was not found.`);
-        }
-      } else {
-        reject('No cards found !');
-      }
-    }, 300);
-  });
-}
-
-export function createCard(card): Promise<void> {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      const cards: Card[] | null = JSON.parse(sessionStorage.getItem('cards'));
-      if(cards && cards.length) {
-        const maxId = Math.max(...cards.map(c => c.id));
-        cards.push({ id: maxId + 1, ...card });
-        sessionStorage.setItem('cards', JSON.stringify(cards));
-        resolve();
-      } else {
-        reject('No cards found !');
-      }
-    }, 300);
-  });
-}
-
-export function updateCard(card): Promise<void> {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      const cards: Card[] | null = JSON.parse(sessionStorage.getItem('cards'));
-      if(cards && cards.length) {
-        const idxToUpdate = cards.findIndex(c => c.id == card.id)
-        if (idxToUpdate >= 0) {
-          cards[idxToUpdate] = card;
-          sessionStorage.setItem('cards', JSON.stringify(cards));
-          resolve();
-        } else {
-          reject(`Impossible to update card with id <${card.id}> because it doesn't exists.`);
-        }
-      } else {
-        reject('No cards found !');
-      }
-    }, 300);
-  });
-}
-
-export function deleteCard(cardId): Promise<void> {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      const cards: Card[] | null = JSON.parse(sessionStorage.getItem('cards'));
-      if(cards && cards.length) {
-        const idxToDelete = cards.findIndex(c => c.id == cardId);
-        if (idxToDelete >= 0) {
-          cards.splice(idxToDelete, 1);
-          sessionStorage.setItem('cards', JSON.stringify(cards));
-          resolve();
-        } else {
-          reject(`Impossible to delete card with id <${cardId}> because it doesn't exists.`);
-        }
-      } else {
-        reject('No cards found !');
-      }
-    });
   });
 }
 
